def1f94b092df0486b40288511d790c2a469193c
Geno1024 <754097987@qq.com>
2015-07-07 12:05:10 +0800

    [add func] add smb2's metadata.

---
+++ src/mario/ost/super_mario_bros_series/super_mario_bros_2/MetaData.java
@@ -0,0 +1,20 @@
+package mario.ost.super_mario_bros_series.super_mario_bros_2;
+
+import mario.ost.meta.Genre;
+
+public class MetaData
+{
+	public static final Genre gameGenre = Genre.Action.Platform;
+
+	public static final int overWorld = 3;
+
+	public static final int underGround = 8;
+
+	public static final int waterWorld = 0;
+
+	public static final int castle = 9;
+
+	public static final int ending = 15;
+
+	public static final int mainTheme = 3;
+}
