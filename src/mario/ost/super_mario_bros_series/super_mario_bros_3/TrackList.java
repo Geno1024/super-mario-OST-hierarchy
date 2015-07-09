ebfc086d5cb08dcf521d301d392557d56e5bc946
Geno1024 <754097987@qq.com>
2015-07-09 12:33:05 +0800

    [add func] add smb3's tracklist

---
+++ src/mario/ost/super_mario_bros_series/super_mario_bros_3/TrackList.java
@@ -0,0 +1,13 @@
+package mario.ost.super_mario_bros_series.super_mario_bros_3;
+
+import java.util.GregorianCalendar;
+
+public class TrackList
+{
+	public static final int trackCount = 36;
+
+	public static final GregorianCalendar releaseDate = new GregorianCalendar(1988, 10, 23);
+
+	public static final String[] composer = {"Kondo Koji"};
+
+}
