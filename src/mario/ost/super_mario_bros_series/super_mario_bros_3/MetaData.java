a882e3bc0974d7e4ba1de5732edf0cfd5552dc7d
Geno1024 <754097987@qq.com>
2015-07-10 18:55:57 +0800

    [add func] add some smb3's metadata

---
+++ src/mario/ost/super_mario_bros_series/super_mario_bros_3/MetaData.java
@@ -0,0 +1,9 @@
+package mario.ost.super_mario_bros_series.super_mario_bros_3;
+
+import mario.ost.meta.Genre;
+
+public class MetaData
+{
+	public static final Genre gameGenre = Genre.Action.Platform;
+
+}
