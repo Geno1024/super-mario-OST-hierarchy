dbb8b4121830acffb4a4597b2ffb178e68b1309c
Geno1024 <754097987@qq.com>
2015-07-16 15:47:49 +0800

    [delete func] delete the null player.

--- src/mario/ost/super_mario_bros_series/super_mario_bros/music_list/Super_Mario_Bros_Main_Theme.java
+++ src/mario/ost/super_mario_bros_series/super_mario_bros/music_list/Super_Mario_Bros_Main_Theme.java
@@ -1,11 +1,8 @@
 package mario.ost.super_mario_bros_series.super_mario_bros.music_list;
 
-import mario.ost.Play;
-
 public class Super_Mario_Bros_Main_Theme
 {
 	public static void main(String[] args)
 	{
-		Play.PlayString(null);
 	}
 }
