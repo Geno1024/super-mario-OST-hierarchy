37fa77d5036ef3b26daab34064a4e11c65c59a6e
Geno1024 <754097987@qq.com>
2015-07-21 17:32:29 +0800

    [add func] add smb3's track name.

--- src/mario/ost/super_mario_bros_series/super_mario_bros_3/TrackList.java
+++ src/mario/ost/super_mario_bros_series/super_mario_bros_3/TrackList.java
@@ -10,4 +10,44 @@ public class TrackList
 
 	public static final String[] composer = {"Kondo Koji"};
 
+	public static final String[] trackName =
+	new String[]
+	{
+		"Grass Land",
+		"Level Theme 1",
+		"Power Up",
+		"Level Clear",
+		"Desert Hill",
+		"Level Theme",
+		"Invincible Starman",
+		"Ocean Side",
+		"Underwater",
+		"Hurry Up",
+		"Spade Puzzle",
+		"Big Island",
+		"Super Mario Rap",
+		"Hammer Brothers",
+		"Music Box",
+		"The Sky",
+		"Coin Heaven",
+		"1 - Up",
+		"Pick A Box",
+		"Magic Warp Whistle",
+		"Iced Land",
+		"Mini - Fortress",
+		"Warp Pipe",
+		"Boom Boom",
+		"Boss Clear",
+		"Pipe Maze",
+		"Airship",
+		"King's Castle",
+		"World Clear",
+		"Castle of Koopa",
+		"Pause",
+		"King Koopa Battle",
+		"King Koopa Falls",
+		"Ending",
+		"1 - Down",
+		"Game Over",
+	};
 }
