20652a024ea98feca559715f4b82a2db804fcd91
Geno1024 <754097987@qq.com>
2015-07-22 21:22:32 +0800

    [add readme] add readme.

---
+++ src/mario/ost/Readme.java
@@ -0,0 +1,8 @@
+package mario.ost;
+
+public class Readme
+{
+	public static final String[] usingLibrary = new String[]{"JFugue"};
+
+	public static final String midiSource = "https://themushroomkingdom.net";
+}
