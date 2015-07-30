d785d5d6137c9ebdd7286c292ec57b9c1cec3046
Geno1024 <754097987@qq.com>
2015-07-31 00:33:56 +0800

    [add func] add smrpg's trackname

--- src/mario/ost/super_mario_rpg_series/super_mario_rpg/TrackList.java
+++ src/mario/ost/super_mario_rpg_series/super_mario_rpg/TrackList.java
@@ -12,7 +12,72 @@ public class TrackList
 
 	public static final boolean monoDisc = false;
 
-	public static final String[] trackName = 
+	public static final String[] trackName_I = 
+	new String[]
 	{
+		"Happy Adventure, Delightful Adventure",
+		"Let's Try",
+		"In The Flower Garden",
+		"Koopa Castle (First Time)",
+		"Fight Against Koopa",
+		"The Sword Descends And The Stars Scatter",
+		"Super Pipe House",
+		"Where Am I Going",
+		"The Road is Full Of Dangers",
+		"Fight Against Monsters",
+		"Victory!!",
+		"Hello, Happy Kingdom",
+		"Explanation!",
+		"A New Partner",
+		"Still, The Road Is Full Of Dangers",
+		"Invincible Star",
+		"Fight Against A Somewhat Stronger Monster",
+		"Here's Some Weapons!",
+		"Fight Against An Armed Boss",
+		"Got A Star Piece",
+		"The Dungeon Is Full Of Monsters",
+		"Let's Go Down The Wine River",
+		"Grandpa And The Delightful Tadpoles",
+		"Shock!",
+		"Sad Song",
+		"Let's Play \"Geno\"",
+		"Geno Awakens",
+		"Beware The Forest's Mushrooms",
+		"Rose Town",
+		"From Inside The Earthern Pipe",
+		"Welcome! Yo'ster Island!",
+		"Let's Race!",
+		"Hard-Working Moles Are Good Moles",
+		"Docaty Mountain Railroad",
+		"Welcome To Bukki Tower",
+		"And My Name's Bukki",
+		"Long Long Ago...",
+		"Heart Beating A Little Faster",
+		"Slope",
+	};
+
+	public static final String[] trackName_II = new String[]
+	{
+		"The Merry Mary Bell Rings",
+		"Celebrational",
+		"The Starlight's Flower",
+		"Sunken Ship",
+		"Going Shopping In Ripple Town",
+		"Fight Against Culex",
+		"Victory Over Culex",
+		"Conversation With Culex",
+		"You And Toadofski Create Exquisite Music",
+		"Let's Do The Fooka-Fooka!",
+		"Margarie Margarita",
+		"Dodo's Coming!!",
+		"Barrel Volcano",
+		"The Axem Rangers Drop In",
+		"Koopa Castle (Second Time)",
+		"Weapons Factory",
+		"Fight Against Kajidoh",
+		"Fight Against Kajidoh, Who Likes Transforming",
+		"Goodbye Geno... ~ Seeing Dreams Through The Window Of The Stars",
+		"Happy Parada, Delightful Parade ~ And The Parade Draws To A Close...",
+		"The End!",
 	};
 }
