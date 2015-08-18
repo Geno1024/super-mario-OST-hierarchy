19201ab11ff71e2effa3f4c5ff2bf0a2957d13da
Geno1024 <754097987@qq.com>
2015-08-19 00:58:41 +0800

    [rewrite] rewrite musictype

--- src/mario/ost/meta/MusicType.java
+++ src/mario/ost/meta/MusicType.java
@@ -2,23 +2,40 @@ package mario.ost.meta;
 
 public class MusicType
 {
-	public MusicType()
-	{}
+	public static final MusicType overWorld = new MusicType("overWorld");
 
-	public static final String overWorld = "overWorld";
+	public static final MusicType underGround = new MusicType("underGround");
 
-	public static final String underGround = "underGround";
+	public static final MusicType waterWorld = new MusicType("waterWorld");
 
-	public static final String waterWorld = "waterWorld";
+	public static final MusicType castle = new MusicType("castle");
 
-	public static final String castle = "castle";
+	public static final MusicType ending = new MusicType("ending");
 
-	public static final String ending = "ending";
+	public static final MusicType mainTheme = new MusicType("mainTheme");
 
-	public static final String mainTheme = "mainTheme";
+	public static final MusicType athletic = new MusicType("athletic");
 
-	public static final String athletic = "athletic";
+	public static final MusicType map = new MusicType("map");
 
-	public static final String map = "map";
+	public String string;
+
+	public MusicType musicType;
+
+	public MusicType(String string)
+	{
+		this.string = string;
+	}
+
+	public MusicType(MusicType musicType)
+	{
+		this.musicType = musicType;
+	}
+
+	@Override
+	public String toString()
+	{
+		return this.string;
+	}
 }
 
