b8793f06c6db56e1db284ad00ab0482cfab15715
Geno1024 <754097987@qq.com>
2015-08-29 01:48:42 +0800

    [rewrite] rewrite genre

--- src/mario/ost/meta/Genre.java
+++ src/mario/ost/meta/Genre.java
@@ -2,14 +2,32 @@ package mario.ost.meta;
 
 public class Genre
 {
-	public Genre(Genre g){}
-
 	public static class Action
 	{
-		public static Genre Platform;
+		public static Genre Platform = new Genre("Platform");
 
-		public static Genre Action;
+		public static Genre Action = new Genre("Action");
 	}
 
-	public static Genre RPG;
+	public static Genre RPG = new Genre("RPG");
+
+	public String genreString;
+
+	public Genre genre;
+
+	public Genre(String genreString)
+	{
+		this.genreString = genreString;
+	}
+
+	public Genre(Genre genre)
+	{
+		this.genre = genre;
+	}
+
+	@Override
+	public String toString()
+	{
+		return genreString;
+	}
 }
