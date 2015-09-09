0dcba76f85eefca0b8395e824dfdbd08b7d2e8ae
Geno1024 <754097987@qq.com>
2015-09-09 20:18:42 +0800

    [add func] add some readme

--- src/mario/ost/Readme.java
+++ src/mario/ost/Readme.java
@@ -5,4 +5,20 @@ public class Readme
 	public static final String[] usingLibrary = new String[]{"JFugue"};
 
 	public static final String midiSource = "https://themushroomkingdom.net";
+
+	public static final class whatToFill
+	{
+		public static final Class<?>[] afterCreatingAPackage =
+		new Class<?>[]
+		{
+			mario.ost.meta.OST.class,
+			mario.ost.meta.Status.class,
+		};
+
+		public static final Class<?>[] afterCreatingASong =
+		new Class<?>[]
+		{
+			mario.ost.meta.SoundTrack.class,
+		};
+	}
 }
