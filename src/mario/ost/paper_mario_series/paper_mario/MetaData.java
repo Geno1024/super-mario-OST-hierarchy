48e55659918d013445c1b76fe4eefb1e39eb2862
Geno1024 <754097987@qq.com>
2015-09-10 20:29:13 +0800

    [add func] add new ost

---
+++ src/mario/ost/paper_mario_series/paper_mario/MetaData.java
@@ -0,0 +1,6 @@
+package mario.ost.paper_mario_series.paper_mario;
+
+public class MetaData
+{
+
+}
---
+++ src/mario/ost/paper_mario_series/paper_mario/TrackList.java
@@ -0,0 +1,10 @@
+package mario.ost.paper_mario_series.paper_mario;
+
+import java.util.GregorianCalendar;
+
+public class TrackList
+{
+	public static final int trackCount = 39 + 39;
+
+	public static final GregorianCalendar releaseDate = new GregorianCalendar(2000, 8, 11);
+}
