14c8384b6b176551fd16603717db736887b2d198
Geno1024 <754097987@qq.com>
2015-09-15 10:18:53 +0800

    [add func] pm composer

--- src/mario/ost/paper_mario_series/paper_mario/TrackList.java
+++ src/mario/ost/paper_mario_series/paper_mario/TrackList.java
@@ -7,4 +7,6 @@ public class TrackList
 	public static final int trackCount = 39 + 39;
 
 	public static final GregorianCalendar releaseDate = new GregorianCalendar(2000, 8, 11);
+
+	public static final String[] composer = {"Yuka Tsujiyoko"};
 }
