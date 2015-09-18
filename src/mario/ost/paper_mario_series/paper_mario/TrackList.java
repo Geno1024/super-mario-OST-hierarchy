ed59946fc91e3caa51042b6a8c784059690008a5
Geno1024 <754097987@qq.com>
2015-09-18 11:19:24 +0800

    [add func] add songs

--- src/mario/ost/paper_mario_series/paper_mario/TrackList.java
+++ src/mario/ost/paper_mario_series/paper_mario/TrackList.java
@@ -15,5 +15,26 @@ public class TrackList
 	public static final String[] trackName =
 	new String[]
 	{
+		"Story of the Stolen Spirits",
+		"Main Title",
+		"A Party at Peach's Castle",
+		"Mario's Theme",
+		"March Ahead",
+		"Goomba Village Theme",
+		"Battle Fanfare",
+		"Hey You!",
+		"Gates of Goomba Castle",
+		"Toad Town THeme",
+		"Shy Guy Riot",
+		"Nice To Meet you",
+		"A Kingdom in Chaos",
+		"Fuzzys Stole My Shell",
+		"Koopa Village Theme",
+		"Koopa Bros. Fortress",
+		"Bill Blaster! Go Faster!",
+		"Trojan Bowser",
+		"Attack of the Koopa Bros.",
+		"Toad Town Variations",
+		"Mt. Rugged Theme",
 	};
 }
